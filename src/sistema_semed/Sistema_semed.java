d65600ada5df4d9a53cc46a1e31c8fb33df1d34c
ti <ti@ti-gestao>
2017-05-25 16:53:04 -0300

    Primeiro commit do projeto do Sistema da SEMED

---
+++ src/sistema_semed/Sistema_semed.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema_semed;
+
+/**
+ *
+ * @author ti
+ */
+public class Sistema_semed {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrm_login login = new JFrm_login();
+        //login.setvisible(true);
+    }
+    
+}
